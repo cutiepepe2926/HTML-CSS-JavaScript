66cac9cefc17eaa3c30759c6b8b1979073431972
cutiepepe2926 <cutiepepe2926@gmail.com>
2025-08-26 12:04:37 +0900

    0826-Second

---
+++ JavaScript/ex07_4.js
@@ -0,0 +1,8 @@
+//&& - 양쪽 항이 모두 true여야 true
+//|| - 양쪽 항중 한쪽만 true여도 true
+let a = true;
+let b = false;
+
+console.log("&&의 결과:" + (a && b) ); //f
+console.log("||의 결과:" + (a || b) ); //t
+console.log("!:" + !a );
