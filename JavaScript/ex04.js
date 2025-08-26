320b7676e9856dc14f5d896d7ca6caeab3329b1e
cutiepepe2926 <cutiepepe2926@gmail.com>
2025-08-26 10:50:42 +0900

    0826-First

---
+++ JavaScript/ex04.js
@@ -0,0 +1,8 @@
+let name = "홍길동"; //string
+let age = 20; //number
+let marathon = 45.125; //number
+let power = false; //boolean
+let nothing = null; //null
+let a; //undefined
+let arr = []; //array
+let obj = {}; //object
---
+++ JavaScript/ex07_1.js
@@ -0,0 +1,14 @@
+//산술연산자 
+let a = 5 * 3;
+let b = 5 / 3; //몫 - 소수몫 까지 구해줌.
+let c = 5 % 3; //나머지
+
+console.log(a, b, c);
+console.log( parseInt(b) ); //실수 -> 정수로 변경
+
+//전위연산 -
+let x = 5;
+let pre = ++x;
+console.log("x는: " + x);
+console.log("pre는: " + pre);
+
