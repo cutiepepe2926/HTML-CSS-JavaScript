b0892295113044ba644c1bfc5a39738812620ed4
cutiepepe2926 <cutiepepe2926@gmail.com>
2025-09-30 10:20:38 +0900

    Delete Java directory

--- Java/Chapter1Chapter7/flowex4.java
+++
@@ -1,9 +0,0 @@
-public class flowex4 {
-    public static void main(String[] args) {
-        int score = 45;
-        char grade = ' ';
-        grade = (score>=90) ? 'A' : (score>=80) ? 'B' : 'C';
-        System.out.println("score is "+grade);
-        
-    }
-}
